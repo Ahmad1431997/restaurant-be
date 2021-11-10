72b9703254cf4914cee36491e212680998d23f3a
unknown <ahmadabuawed@gmail.com>
2021-11-10 19:19:50 +0200

    menuList

--- routes/dishRoutes.js
+++ routes/dishRoutes.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const passport = require("passport");
 const router = express.Router();
-const { dishesList } = require("../controllers/dishControllers");
+const { dishesList, deleteDish, updateDish } = require("../controllers/dishControllers");
+const upload = require("../media/middleware/multer");
 
 
 
 router.get("/dishes", dishesList);
+
+router.delete("/dishes/:dishId",deleteDish)
+
+router.put("/dishes/:dishId",upload.single("image"),updateDish)
 
 module.exports = router;
