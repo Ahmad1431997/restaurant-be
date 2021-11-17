1c345466358c7bfeb43dcf0f2da379bc86e5087e
unknown <ahmadabuawed@gmail.com>
2021-11-17 13:42:13 +0200

    reservation2

--- routes/reservationRoutes.js
+++ routes/reservationRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const passport = require("passport");
-const { addReservation } = require("../controllers/reservationControllers");
+const { addReservation, reservationsList } = require("../controllers/reservationControllers");
 const router = express.Router();
 //controllers
 
@@ -10,6 +10,6 @@ router.post(
   passport.authenticate("jwt", { session: false }),
   addReservation
 );
-
+router.get("/reservations",passport.authenticate("jwt",{session:false}),reservationsList)
 
 module.exports = router;
